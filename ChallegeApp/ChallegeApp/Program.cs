fab7af6a8f585df84659959563166b86813867ac
Thobinebri <matura.lukasz@gmail.com>
2023-05-28 16:50:50 +0200

    Zmienne

--- ChallegeApp/ChallegeApp/Program.cs
+++ ChallegeApp/ChallegeApp/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Z opóźnieniem ale jest;-) Hello, World-nowe 28.05.2023 16:13!");
+//var myAge = 49;
+int myAge2 = int.MaxValue;
+long myAge3 = long.MaxValue;
+float myNumber = float.MaxValue;
+double myNumber2 = double.MaxValue;
+
+//Console.WriteLine("Z opóźnieniem ale jest;-) Hello, World-nowe 28.05.2023 16:13!");
+Console.WriteLine(myNumber2);
