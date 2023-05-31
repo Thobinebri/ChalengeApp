efc1e2708eed0e0a2d419e475afd7a077720ad6c
Thobinebri <matura.lukasz@gmail.com>
2023-05-31 15:52:56 +0200

    Dzien 4

--- ChallegeApp/ChallegeApp/Program.cs
+++ ChallegeApp/ChallegeApp/Program.cs
@@ -1,9 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //var myAge = 49;
-int myAge2 = int.MaxValue;
+//liczy całkowite
+int myAge2 = 49;
 long myAge3 = long.MaxValue;
+
+//liczby zmiennoprzecinkowe
 float myNumber = float.MaxValue;
 double myNumber2 = double.MaxValue;
 
+// zmienne tekstowe
+string name = "Luk";
+string surname = "Matura";
+char myFirst = 'A';
+var result2 = name.ToArray();
+bool isActive = false;
+
+
+
 //Console.WriteLine("Z opóźnieniem ale jest;-) Hello, World-nowe 28.05.2023 16:13!");
-Console.WriteLine(myNumber2);
+if(surname == "Matura")
+    Console.WriteLine("Jest prawda");
+else
+    Console.WriteLine("False");
