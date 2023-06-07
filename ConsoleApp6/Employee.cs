17da7179d92b1af4472466c438992a3412f074b2
Thobinebri <matura.lukasz@gmail.com>
2023-06-07 19:15:29 +0200

    Dzien 6: Class Employee i Program

---
+++ ConsoleApp6/Employee.cs
@@ -0,0 +1,52 @@
+﻿namespace ConsoleApp6
+{
+
+    public class Employee
+    {
+        public static string Fabryka = "3Com";
+
+        private string password = null;
+        private string name;
+        //    private int score;
+        private List<int> score = new List<int>();
+        public string Login { get; private set; }
+        public string Name { get; private set; }
+        public string lastName { get; private set; }
+
+
+        public Employee(string login)
+        {
+            this.Login = login;
+        }
+        public Employee(string login, string name)
+        {
+            this.Login = login;
+            this.Name = name;
+        }
+        public Employee(string login, string lastname, int score)
+        {
+            this.Login = login;
+            this.lastName = lastname;
+            //        this.score = score;
+        }
+        public Employee(string login, string name, string lastname)
+        {
+            this.Login = login;
+            this.name = name;
+            this.lastName = lastname;
+        }
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+        public void AddScore(int score)
+        {
+            this.score.Add(score);
+        }
+
+
+    }
+}
--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Dzień 6.");
+﻿using ConsoleApp6;
+
+var user1 = new Employee("Lukasz", "Matura", 1);
+var user2 = new Employee("Adam", "Kowalski", 15);
+var user3 = new Employee("Ewa", "Nowak", 3);
+//Console.Write(user1.name + " " + user1.lastName + " 1 ");
+user1.AddScore(5);
+user1.AddScore(6);
+Console.Write(user1.Result + " 1 ");
+Console.WriteLine(Employee.Fabryka);
+
+
+
+
+//Testy
+//Console.WriteLine(user1.login+" 1 "+ user1.ToString);
