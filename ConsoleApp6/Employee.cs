21d1ba6838239b679ef768f73d0d8a4f611ea961
Thobinebri <matura.lukasz@gmail.com>
2023-06-11 13:28:00 +0200

    Dzien 6_v2

--- ConsoleApp6/Employee.cs
+++ ConsoleApp6/Employee.cs
@@ -3,48 +3,54 @@
 
     public class Employee
     {
-        public static string Fabryka = "3Com";
-
-        private string password = null;
-        private string name;
-        //    private int score;
-        private List<int> score = new List<int>();
-        public string Login { get; private set; }
-        public string Name { get; private set; }
-        public string lastName { get; private set; }
+        public static string Company = "3Com";
+        private string Name;
+        private string Surname;
+        private int Age;
+        private List<int> Score = new List<int>();
 
 
-        public Employee(string login)
+
+        public Employee(string name, string surname, int age)
         {
-            this.Login = login;
-        }
-        public Employee(string login, string name)
-        {
-            this.Login = login;
             this.Name = name;
-        }
-        public Employee(string login, string lastname, int score)
-        {
-            this.Login = login;
-            this.lastName = lastname;
-            //        this.score = score;
-        }
-        public Employee(string login, string name, string lastname)
-        {
-            this.Login = login;
-            this.name = name;
-            this.lastName = lastname;
+            this.Surname = surname;
+            this.Age = age;
         }
         public int Result
         {
             get
             {
-                return this.score.Sum();
+                return this.Score.Sum();
             }
         }
+        public int age
+        {
+            get
+            {
+                return this.Age;
+            }
+        }
+ 
+        public string name
+        {
+            set { Name = name; }
+            get
+            {
+                return Name;
+            }
+        }
+        public string surname
+        {
+            get
+            {
+                return Surname;
+            }
+        }
+
         public void AddScore(int score)
         {
-            this.score.Add(score);
+            this.Score.Add(score);
         }
 
 
--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,16 +1,86 @@
 ﻿using ConsoleApp6;
+using System.Diagnostics;
 
-var user1 = new Employee("Lukasz", "Matura", 1);
-var user2 = new Employee("Adam", "Kowalski", 15);
-var user3 = new Employee("Ewa", "Nowak", 3);
-//Console.Write(user1.name + " " + user1.lastName + " 1 ");
+var user1 = new Employee("Lukasz", "Matura", 25);
+var user2 = new Employee("Adam", "Kowalski", 30);
+var user3 = new Employee("Ewa", "Nowak", 39);
+List<Employee> lista = new List<Employee>();
+List<int> TheBest = new List<int>();
+lista.Add(user1);
+lista.Add(user2);
+lista.Add(user3);
+
+user1.AddScore(10);
+user1.AddScore(4);
+user1.AddScore(0);
 user1.AddScore(5);
 user1.AddScore(6);
-Console.Write(user1.Result + " 1 ");
-Console.WriteLine(Employee.Fabryka);
 
+user2.AddScore(4);
+user2.AddScore(7);
+user2.AddScore(4);
+user2.AddScore(0);
+user2.AddScore(7);
 
+user3.AddScore(8);
+user3.AddScore(4);
+user3.AddScore(8);
+user3.AddScore(5);
+user3.AddScore(8);
+
+Console.WriteLine("Nazwa firmy: " + Employee.Company);
+int i = 0;
+int iMax = 0;
+int scoreMax = 0;
+
+foreach (var employee in lista)
+{
+    Console.Write(lista[i].name+" ");
+    Console.Write(lista[i].surname);
+    Console.Write(" Wiek: " + lista[i].age);
+    Console.WriteLine(" Score: " + lista[i].Result);
+    TheBest.Add(lista[i].Result);
+    if (lista[i].Result > scoreMax)
+    {
+        iMax = i;
+        scoreMax = lista[i].Result;
+    }
+    ++i;
+}
+Console.Write("Najlepszy to: " + lista[iMax].name + " "+lista[iMax].surname+" wiek:" + lista[iMax].age);
+Console.WriteLine(", a wynik to: " + scoreMax);
 
 
 //Testy
+/*
+Boolean zakres = false;
+Boolean dodaj = true;
+while (dodaj == true)
+{
+    while (zakres == false)
+    {
+        Console.WriteLine("Dodaj imię pracownika. q - zakończ dodawanie");
+        if (Console.ReadLine() == "q")
+            dodaj = false; break;
+        int a = Int32.Parse(Console.ReadLine());
+
+
+        if (a < 0 || a > 10)
+            Console.WriteLine("Ocena musi być z zakresu 0-10");
+        else
+            zakres = true;
+
+    }
+}
+*/
+
+
 //Console.WriteLine(user1.login+" 1 "+ user1.ToString);
+//Console.Write(user1.name + " " + user1.lastName + " 1 ");
+
+//int a = 23;
+//string b;
+
+//b = Console.ReadLine();
+//a = Int32.Parse(b);
+//Console.WriteLine(b);
